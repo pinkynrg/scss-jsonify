6a398df69f4c7bcfec164df8379ac5553c4b9334
Francesco Meli <francesco.meli@beefree.io>
2024-09-05 11:52:53 +0200

    fix: clean up useless files

--- src/regex.ts
+++
@@ -1,9 +0,0 @@
-export const selX = /([^\s\;\{\}][^\;\{\}]*)\{/g;
-export const endX = /\}/g;
-export const lineX = /([^\;\{\}]*)\;/g;
-export const commentX = /\/\*[\s\S]*?\*\//g;
-export const lineAttrX = /([^\:]+):([^\;]*);/;
-
-// This is used, a concatenation of all above. We use alternation to
-// capture.
-export const altX = /(\/\*[\s\S]*?\*\/)|([^\s\;\{\}][^\;\{\}]*(?=\{))|(\})|([^\;\{\}]+\;(?!\s*\*\/))/gim;
