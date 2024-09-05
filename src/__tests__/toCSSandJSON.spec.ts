7ef035909008e6d01a099fd553638b08c4f07a18
Francesco Meli <francesco.meli@beefree.io>
2024-09-05 10:34:40 +0200

    test: update test

--- src/__tests__/toCSSandJSON.spec.ts
+++ src/__tests__/toCSSandJSON.spec.ts
@@ -2,34 +2,34 @@ import fs from 'fs';
 import { toCSS, toJSON } from '../index';
 
 [
-  // 'test-0', 
-  // 'test-1', 
-  'test-2'
+  'test-0', 
+  'test-1', 
+  // 'test-2'
 ].forEach((test: string) => {
 
   const cssFileContent = fs.readFileSync(`${__dirname}/${test}.css`, 'utf8');
   const jsonFileContent = JSON.parse(fs.readFileSync(`${__dirname}/${test}.json`, 'utf8'))
 
-  it('should return the expected css from json', async () => {
+  it(`${test} - should return the expected css from json`, async () => {
     const builtJson = toJSON(cssFileContent)
     expect(builtJson).toEqual(jsonFileContent)
   });
 
-  // it('should return the expected css from json', async () => {
-  //   const builtCSS = toCSS(jsonFileContent)
-  //   expect(builtCSS).toBe(cssFileContent)
-  // });
+  it(`${test} - should return the expected css from json`, async () => {
+    const builtCSS = toCSS(jsonFileContent)
+    expect(builtCSS).toBe(cssFileContent)
+  });
 
-  // it('should match itself if converted to json and back to css', async () => {
-  //   const builtJson = toJSON(cssFileContent)
-  //   const backtoCSS = toCSS(builtJson)
-  //   expect(backtoCSS).toEqual(cssFileContent)
-  // })
+  it(`${test} - should match itself if converted to json and back to css`, async () => {
+    const builtJson = toJSON(cssFileContent)
+    const backtoCSS = toCSS(builtJson)
+    expect(backtoCSS).toEqual(cssFileContent)
+  })
 
-  // it('should match itself if converted to css and back to json', async () => {
-  //   const builtCSS = toCSS(jsonFileContent)
-  //   const backtoJson = toJSON(builtCSS)
-  //   expect(backtoJson).toEqual(jsonFileContent)
-  // })
+  it(`${test} - should match itself if converted to css and back to json`, async () => {
+    const builtCSS = toCSS(jsonFileContent)
+    const backtoJson = toJSON(builtCSS)
+    expect(backtoJson).toEqual(jsonFileContent)
+  })
 
 })
